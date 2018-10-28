81b49313792e1cdd4407add23b3abffebd6b68ab
李志钟 <aichaellee@lizhizhongdeMacBook-Pro.local>
2018-10-28 23:24:16 +0800

    init spring cloud framework

---
+++ order/server/src/main/java/com/imooc/order/config/GirlConfig.java
@@ -0,0 +1,18 @@
+package com.imooc.order.config;
+
+import lombok.Data;
+
+/**
+ * Created by 廖师兄
+ * 2018-02-03 22:25
+ */
+@Data
+//@Component
+//@ConfigurationProperties("girl")
+//@RefreshScope
+public class GirlConfig {
+
+    private String name;
+
+    private Integer age;
+}
